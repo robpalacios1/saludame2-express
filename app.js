89b6f09529667c28d72b45d6409808fe3d79c948
Roberto <robpalacios1@hotmail.com>
2019-10-23 22:27:36 -0500

    capitalize a string

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/makers/:name', (req, res) => {
+    app.get('/makers/:name', (req, res) => {
 
-    res.send(`<h1>Hola ${req.params.name.toUpperCase()}!</h1>`)
+        res.send(`<h1>Hola ${req.params.name.charAt(0).toUpperCase() + req.params.name.slice(1)}!</h1>`);
 
-});
+    });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
